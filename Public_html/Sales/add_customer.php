2048b2bee8396ab4029deccf04cc3907e8c5bf9a
kevinfikke <kevinfikke1@hotmail.com>
2016-10-31 12:37:46 +0100

    toegevoegd add en edit customers
    2 pagina's toegevoegd in sales

---
+++ Public_html/Sales/add_customer.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Startklaar
+ * Date: 31-10-2016
+ * Time: 11:19
+ */
+
+?>
+
+<h1>add customer</h1>
+
+<img src="" alt="">
+
+
+<div class="form-group">
+    <label for="company-name">company name</label>
+    <input type="text" name="company-name" id="company-name">
+</div>
+
+<div class="form-group">
+    <label for="adres"> company adres</label>
+    <input type="text" name="adres" id="adres">
+</div>
+
+<div class="form-group">
+    <label for="house-number">house number</label>
+    <input type="text" name="house-number" id="house-number">
+</div>
+
+<div class="form-group">
+    <label for="zip-code">Zip code</label>
+    <input type="text" name="zip-code" id="zip-code">
+</div>
+
+<div class="form-group">
+    <label for="city">city</label>
+    <input type="text" name="city" id="city">
+</div>
+
+<div class="form-group">
+    <label for="second-adres">second adres</label>
+    <input type="text" name="second-adres" id="second-adres">
+</div>
+
+<div class="form-group">
+    <label for="second-house-number">second house number</label>
+    <input type="text" name="second-house-number" id="second-house-number">
+</div>
+
+<div class="form-group">
+    <label for="second-zip-code">second zip code</label>
+    <input type="text" name="second-zip-code" id="second-zip-code">
+</div>
+
+<div class="form-group">
+    <label for="second-city">second city</label>
+    <input type="text" name="second-city" id="second-city">
+</div>
+
+<div class="form-group">
+    <label for="contact-person">contact person</label>
+    <input type="text" name="contact-person" id="contact-person">
+</div>
+
+<div class="form-group">
+    <label for="telephone-number"> telephone number</label>
+    <input type="text" name="telephone-number" id="telephone-number">
+</div>
+
+<div class="form-group">
+    <label for="second-telephone-number"></label>
+    <input type="text" name="second-telephone-number" id="second-telephone-number">
+</div>
+
+<div class="form-group">
+    <label for="fax-number">fax number</label>
+    <input type="text" name="fax-number" id="fax-number">
+</div>
+
+<div class="form-group">
+    <label for="mail-adres"> mail adres</label>
+    <input type="text" name="mail-adres" id="mail-adres">
+</div>
+
+<select name="potential customer" id="potential customer">
+    <option value=""> </option>
+    <option value="yes">yes</option>
+    <option value="no">no</option>
+</select>
